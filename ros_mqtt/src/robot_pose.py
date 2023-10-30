aebcb24d98e691f04df9d0aab623b76bcb6db115
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-10-29 20:07:37 -0700

    Update robot_pose.py

--- ros_mqtt/src/robot_pose.py
+++ ros_mqtt/src/robot_pose.py
@@ -2,58 +2,6 @@
 
 # THIS SCRIPT SEND THE POSE OF ROBOT TO MQTT SERVER
 
-# import rospy
-# from geometry_msgs.msg import PoseWithCovarianceStamped
-# import paho.mqtt.client as mqtt
-# import json
-# from tf.transformations import euler_from_quaternion
-
-# # MQTT broker details
-# mqtt_broker_host = "10.14.7.175"
-# mqtt_broker_port = 1883
-# mqtt_username = "mascot"
-# mqtt_password = "demo"
-# mqtt_topic = "robot/pose" 
-# mqtt_client = mqtt.Client()
-
-# def amcl_pose_callback(msg):
-#     pose = msg.pose.pose
-#     orientation_quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
-#     roll, pitch, yaw = euler_from_quaternion(orientation_quaternion)
-    
-#     pose_data = {
-#         "pose_position": {
-#             "x": pose.position.x,
-#             "y": pose.position.y,
-#             "z": pose.position.z
-#         },
-#         "orientation": {
-#             "roll": roll,
-#             "pitch": pitch,
-#             "yaw": yaw
-#         }
-#     }
-#     pose_data_json = json.dumps(pose_data) 
-#     mqtt_client.publish(mqtt_topic, pose_data_json, qos=0)  
-
-# def main():
-#     rospy.init_node('amcl_pose', anonymous=True)
-#     rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, amcl_pose_callback)
-
-#     mqtt_client.username_pw_set(username=mqtt_username, password=mqtt_password)
-#     mqtt_client.connect(mqtt_broker_host, mqtt_broker_port)
-
-#     try:
-#         mqtt_client.loop_start()  
-#         rospy.spin()
-#     except KeyboardInterrupt:
-#         print("Shutting down")
-#         mqtt_client.disconnect()
-
-# if __name__ == '__main__':
-#     main()
-
-
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import json
