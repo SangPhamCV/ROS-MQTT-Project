c955bdd820122e64645d0c3a810a725400f296ff
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-10-29 20:07:12 -0700

    Update points_make_path.py

--- ros_mqtt/src/points_make_path.py
+++ ros_mqtt/src/points_make_path.py
@@ -1,60 +1,6 @@
 #!/usr/bin/env python3
 
-# THIS SCRIPT CALLS A SERVICE THAT HANDLES ALL THE POINTS, MAKING UP THE PATH
-
-
-# import rospy
-# from ros_mqtt.srv import GetNavPath, GetNavPathRequest
-# from move_base_msgs.msg import MoveBaseActionGoal
-# import paho.mqtt.client as mqtt
-# import json
-# import time
-
-# # MQTT broker details
-# mqtt_broker_host = "10.14.7.175"
-# mqtt_broker_port = 1883
-# mqtt_username = "mascot"
-# mqtt_password = "demo"
-# mqtt_topic = "robot/point_path"  
-# mqtt_client = mqtt.Client()
-
-# path_data = {}  # Initialize an empty dictionary
-
-# def call_path_service():
-#     rospy.wait_for_service('get_path')
-#     try:
-#         get_path = rospy.ServiceProxy('get_path', GetNavPath)
-#         response = get_path(GetNavPathRequest())
-#         time.sleep(0.5)
-
-#         poses = response.get_path.poses
-#         positions = [pose.pose.position for pose in poses]
-#         path_data["positions"] = [{"x": round(p.x, 2), "y": round(p.y, 2)} for p in positions]
-#         print(len(positions))
-        
-#         path_data_json = json.dumps(path_data)
-#         mqtt_client.publish(mqtt_topic, str(path_data_json), qos=0)
-#     except rospy.ServiceException as e:
-#         rospy.logerr("Service call failed: %s", e)
-
-# def goal_pose_callback(msg):
-#     goal_pose = msg.goal.target_pose.pose
-#     _ = goal_pose.position
-#     call_path_service()
-
-# if __name__ == '__main__':
-#     rospy.init_node('call_service_handle')
-#     rospy.Subscriber('/move_base/goal', MoveBaseActionGoal, goal_pose_callback)
-    
-#     mqtt_client.username_pw_set(username=mqtt_username, password=mqtt_password)
-#     mqtt_client.connect(mqtt_broker_host, mqtt_broker_port)
-
-#     try:
-#         mqtt_client.loop_start()
-#         rospy.spin()
-#     except KeyboardInterrupt:
-#         print("Shutting down")
-#         mqtt_client.disconnect()    
+# THIS SCRIPT CALLS A SERVICE THAT HANDLES ALL THE POINTS, MAKING UP THE PATH 
 
 import rospy
 from ros_mqtt.srv import GetNavPath, GetNavPathRequest
