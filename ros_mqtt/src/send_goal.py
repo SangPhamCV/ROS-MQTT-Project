f63b913413309e6065e5fdfc9c711ee98bf87f9f
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-10-29 20:05:49 -0700

    Add files via upload

---
+++ ros_mqtt/src/send_goal.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+#THIS SCRIPT SEND THE GOAL PLAN OF ROBOT
+
+import rospy
+import actionlib
+from std_msgs.msg import Float64MultiArray
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import math
+
+class MoveBaseClientNode:
+    def __init__(self):
+    
+        self.x_value = None
+        self.y_value = None
+        self.theta_value = None
+
+        rospy.init_node('movebase_client_py')
+        rospy.Subscriber('ros_topic', Float64MultiArray, self.callback)
+
+        self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.client.wait_for_server()
+
+    def callback(self, data):
+
+        received_list = data.data
+        rospy.loginfo("Received list: %s", received_list)
+        self.x_value, self.y_value, self.theta_value = received_list
+
+    def movebase_client(self, value_x, value_y, value_theta):
+        
+        self.theta_radian = math.pi/180*value_theta
+        self.z_value = math.sin(self.theta_radian/2)
+        self.w_value = math.cos(self.theta_radian/2)
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = value_x
+        goal.target_pose.pose.position.y = value_y
+        goal.target_pose.pose.orientation.z = self.z_value
+        goal.target_pose.pose.orientation.w = self.w_value
+
+        self.client.send_goal(goal)
+        wait = self.client.wait_for_result()
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available!")
+        else:
+            return self.client.get_result()
+
+    def run(self):
+
+        while not rospy.is_shutdown():
+            if all(getattr(self, attr) is not None for attr in ['x_value', 'y_value', 'theta_value']):
+                
+                try:
+                    result = self.movebase_client(self.x_value, self.y_value, self.theta_value)
+                    if result:
+                        rospy.loginfo("Goal execution done!")
+                    else:
+                        rospy.loginfo("Goal execution failed!")
+                except rospy.ROSInterruptException:
+                    rospy.loginfo("Navigation test finished.")
+
+                self.x_value = None
+                self.y_value = None
+                self.theta_value = None
+
+        
+if __name__ == '__main__':
+
+    move_base_node = MoveBaseClientNode()
+    move_base_node.run()
---
+++ ros_mqtt/src/test_define.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# THIS SCRIPT SEND THE POSE OF ROBOT TO MQTT SERVER
+
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+import json
+from tf.transformations import euler_from_quaternion
+from define_mqtt import DefineMqtt
+
+define_mqtt = DefineMqtt()
+
+define_mqtt.define()
+
+topic_mqtt = rospy.get_param('topic_mqtt', "robot/pose")
+
+def amcl_pose_callback(msg):
+    pose = msg.pose.pose
+    orientation_quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    roll, pitch, yaw = euler_from_quaternion(orientation_quaternion)
+    
+    pose_data = {
+        "pose_position": {
+            "x": pose.position.x,
+            "y": pose.position.y,
+            "z": pose.position.z
+        },
+        "orientation": {
+            "roll": roll,
+            "pitch": pitch,
+            "yaw": yaw
+        }
+    }
+    pose_data_json = json.dumps(pose_data) 
+    define_mqtt.mqtt_client.publish(topic_mqtt, pose_data_json, qos=0)  
+
+def main():
+    rospy.init_node('amcl_pose', anonymous=True)
+    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, amcl_pose_callback)
+
+    try:
+        define_mqtt.mqtt_client.loop_start()  
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+        define_mqtt.mqtt_client.disconnect()
+
+if __name__ == '__main__':
+    main()
