db38ee5bae00f33b00c74df5c30d9fa0c01f45df
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-11-19 18:43:27 -0800

    Update cal_distance.py

--- ros_mqtt/src/cal_distance.py
+++ ros_mqtt/src/cal_distance.py
@@ -19,13 +19,13 @@ define_mqtt = DefineMqtt()
 
 define_mqtt.define()
 
-topic_mqtt = rospy.get_param('topic_mqtt_cal', "robot/cal")
+topic_mqtt = rospy.get_param('mqtt_pub_estimate', "robot/estimate")
 
 def callback(path_msg):
     global path_length
     path_length = 0
 
-    for i in range(len(path_msg.poses) - 1):    # calculate distance fomular
+    for i in range(len(path_msg.poses) - 1):        # calcutate distance fomular
         position_a_x = path_msg.poses[i].pose.position.x
         position_b_x = path_msg.poses[i+1].pose.position.x
         position_a_y = path_msg.poses[i].pose.position.y
@@ -37,7 +37,7 @@ def callback(path_msg):
     actual_distance = distance_array[-1]
     estimate_distance = distance_array[0]
     time_estimation = estimate_distance / 0.8 # 0.8 is avg velocity of robot
-    # when the robot's pose has changed, clead the path_distance and recalculated
+    # when the robot's pose has changed, clear the path_distance and recalculated
     if goal_pose_callback() is False:
         distance_array.clear()
     
@@ -46,6 +46,7 @@ def callback(path_msg):
         "time_estimation": time_estimation,
         "actual_distance": actual_distance
     }
+    
     path_distance_json = json.dumps(path_distance)
     define_mqtt.mqtt_client.publish(topic_mqtt, path_distance_json, qos=0)
 
@@ -55,15 +56,15 @@ def goal_pose_callback():
     if previous_pose is None:
         previous_pose = Pose()        
         return True 
+    if current_goal is not None:
+        current_pose = current_goal.goal.target_pose.pose
+        if previous_pose is None:
+            previous_pose = deepcopy(current_pose)
+            return True 
 
-    current_pose = current_goal.goal.target_pose.pose
-    if previous_pose is None:
-        previous_pose = deepcopy(current_pose)
-        return True 
-
-    if current_pose != previous_pose:
-        previous_pose = deepcopy(current_pose)
-        return False 
+        if current_pose != previous_pose:
+            previous_pose = deepcopy(current_pose)
+            return False 
 
 def current_goal_callback(msg):
     global current_goal
