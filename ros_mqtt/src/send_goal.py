7a68839c33dcc5457ccd217ea12cba3a25c5bee2
SangPhamCV <147231602+SangPhamCV@users.noreply.github.com>
2023-11-19 18:23:58 -0800

    Update send_goal.py

--- ros_mqtt/src/send_goal.py
+++ ros_mqtt/src/send_goal.py
@@ -1,34 +1,43 @@
 #!/usr/bin/env python3
 
-#THIS SCRIPT SEND THE GOAL PLAN OF ROBOT
-
 import rospy
 import actionlib
 from std_msgs.msg import Float32MultiArray
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import math
+from define_mqtt import DefineMqtt  # Import the DefineMqtt module
 
 class MoveBaseClientNode:
     def __init__(self):
-    
         self.x_value = None
         self.y_value = None
         self.theta_value = None
+        self.run_request = None
 
         rospy.init_node('movebase_client_py')
         rospy.Subscriber('/goal_point', Float32MultiArray, self.callback)
 
         self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.client.wait_for_server()
+        # Create an instance of DefineMqtt
+        self.define = DefineMqtt()
+        
+        self.mqtt_sub_move = rospy.get_param('mqtt_sub_move', 'robot/eheheh')
+
+        self.define.define()
+        self.define.mqtt_client.on_message = self.on_mqtt_message
+        self.define.mqtt_client.subscribe(self.mqtt_sub_move)
+        self.define.mqtt_client.loop_start()
 
     def callback(self, data):
-
         received_list = data.data
         self.x_value, self.y_value, self.theta_value = received_list
 
+    def on_mqtt_message(self, client, userdata, msg):
+        self.run_request = msg.payload.decode()
+
     def movebase_client(self, value_x, value_y, value_theta):
-        
-        self.theta_radian = math.pi/180*value_theta
+        self.theta_radian = math.pi/180 * value_theta
         self.z_value = math.sin(self.theta_radian/2)
         self.w_value = math.cos(self.theta_radian/2)
 
@@ -41,28 +50,33 @@ class MoveBaseClientNode:
         goal.target_pose.pose.orientation.w = self.w_value
 
         self.client.send_goal(goal)
-        wait = self.client.wait_for_result()
-        if not wait:
-            rospy.logerr("Action server not available!")
-            rospy.signal_shutdown("Action server not available!")
-        else:
-            return self.client.get_result()
+
+        rospy.loginfo("Executing goal...")
+        while not rospy.is_shutdown():
+            if self.run_request.lower() == "cancel":
+                self.client.cancel_goal()
+                rospy.loginfo("Goal execution canceled.")
+                self.run_request = None
+                break
+
+            if self.client.get_state() in [actionlib.GoalStatus.SUCCEEDED, actionlib.GoalStatus.ABORTED]:
+                rospy.loginfo("Goal execution done!")
+                break
 
     def run(self):
-
         while not rospy.is_shutdown():
             if all(getattr(self, attr) is not None for attr in ['x_value', 'y_value', 'theta_value']):
-                
                 try:
-                    run_request = input("Execute the path (OK)? ").lower()
-                    if run_request == "ok":
+                    self.run_request = None
+                    print("Execute the path (start)? ")
+                    while self.run_request is None:
+                        rospy.sleep(0.1)
+                    if self.run_request.lower() == "start":
                         result = self.movebase_client(self.x_value, self.y_value, self.theta_value)
-                        if result:
-                            rospy.loginfo("Goal execution done!")
-                        else:
-                            rospy.loginfo("Goal execution failed!")
                     else:
                         rospy.loginfo("Path execution canceled.")
+
+                    self.run_request = None
                 except rospy.ROSInterruptException:
                     rospy.loginfo("Navigation test finished.")
 
@@ -70,8 +84,6 @@ class MoveBaseClientNode:
                 self.y_value = None
                 self.theta_value = None
 
-        
 if __name__ == '__main__':
-
     move_base_node = MoveBaseClientNode()
     move_base_node.run()
